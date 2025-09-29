d1779e3a61316d01e42872f22e0ae59b87937309
lifelover <sargssss@gmail.com>
2025-09-29 10:54:19 +0300

    add new colors

--- app/src/main/java/com/lifelover/companion159/presentation/theme/Color.kt
+++ app/src/main/java/com/lifelover/companion159/presentation/theme/Color.kt
@@ -2,10 +2,36 @@ package com.lifelover.companion159.presentation.theme
 
 import androidx.compose.ui.graphics.Color
 
+// Military Dark Theme Colors
+val MilitaryGreen = Color(0xFF2D4A2B)
+val MilitaryGreenLight = Color(0xFF3D5A3B)
+val MilitaryOlive = Color(0xFF4A5D39)
+val MilitaryTan = Color(0xFF8B7D6B)
+val MilitaryKhaki = Color(0xFFA89F91)
+
+val MilitaryDarkGray = Color(0xFF1C1C1C)
+val MilitaryGray = Color(0xFF2E2E2E)
+val MilitaryLightGray = Color(0xFF4A4A4A)
+
+val MilitaryRed = Color(0xFFB71C1C)
+val MilitaryYellow = Color(0xFFF9A825)
+val MilitaryWhite = Color(0xFFE0E0E0)
+
+// Status Colors
+val StatusSuccess = Color(0xFF2E7D32)
+val StatusError = Color(0xFFB71C1C)
+val StatusWarning = Color(0xFFF57F17)
+val StatusInfo = Color(0xFF1976D2)
+
+// Military Light Theme Colors (for contrast)
+val MilitaryBeige = Color(0xFFD7CCC8)
+val MilitaryLightTan = Color(0xFFEFEBE9)
+val MilitarySand = Color(0xFFF5F5F5)
+
+// Legacy colors (keep for compatibility)
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
-
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
